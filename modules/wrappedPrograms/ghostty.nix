c571d3ece86ef94cc2b126bc1d5779b7d303c6f0
mise_png <semihoficial@gmail.com>
2026-06-01 02:53:37 +0300

    - added cursor shader to ghostty -

--- modules/wrappedPrograms/ghostty.nix
+++ modules/wrappedPrograms/ghostty.nix
@@ -4,9 +4,19 @@
   ...
 }: {
   perSystem = {pkgs, ...}: let
+    cursor-shaders = pkgs.fetchFromGitHub {
+      owner = "sahaj-b";
+      repo = "ghostty-cursor-shaders";
+      rev = "06d4e90fb5410e9c4d0b3131584060adddf89406";
+
+      hash = "sha256-G/UIr1bKnxn1AcHl/4FL/jou6b7M2VeREslYVELxdmw=";
+    };
+
     ghosttyConfig =
       pkgs.writeText "config.ghostty"
       ''
+        custom-shader = ${cursor-shaders}/cursor_tail.glsl
+
         keybind = alt+1=goto_tab:1
         keybind = alt+2=goto_tab:2
         keybind = alt+3=goto_tab:3
